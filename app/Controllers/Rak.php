a10ddac5ce1279c94a5b5b05ba97199c4d781890
mdjajas <181111071@mhs.stiki.ac.id>
2021-04-09 13:08:11 +0700

    tambah

---
+++ app/Controllers/Rak.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controllers;
+
+class Rak extends BaseController
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    //--------------------------------------------------------------------
+
+    private function array_default()
+    {
+        return array(
+            'header_title' => 'Data Rak Buku',
+            'badges' => 'Pages Data Rak Buku',
+            'sidebar' => 4,
+            'link_breadcrumb' => route_to('view_rak')
+        );
+    }
+
+    //--------------------------------------------------------------------
+
+    public function index()
+    {
+
+        $query = $this->query->query_rak_show_all();
+        $dataset = $this->model->queryArray($query);
+        $components = array(
+            'is_show_badge4' => false,
+            'link_add' => route_to('view_add_rak'),
+            'desc_badges' => 'Berikut adalah daftar semua data kategori yang terdaftar',
+            'dataset' => $dataset,
+        );
+
+        return view('admin/pages/layouts/rak', array_merge($this->array_default(), $components));
+    }
+}
